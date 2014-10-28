28bf2ddf87d54e6a90f143a558b9ca154bccfbf7
capetillorob <capetillorob@hotmail.com>
2014-10-27 19:03:27 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -155,59 +155,59 @@
             
                         
             }
-    }
-    
-    // move and loop the bushes
-    
-    for (CCNode *bush in _bushes) {
+        // move and loop the bushes
         
-        // move the bush
-        
-        bush.position = ccp(bush.position.x -
-                            
-                            (character.physicsBody.velocity.x * delta), bush.position.y);
-        
-        
-        
-        // if the left corner is one complete width off the screen,
-        
-        // move it to the right
-        
-        if (bush.position.x <= (-1 * bush.contentSize.width)) {
+        for (CCNode *bush in _bushes) {
             
-            bush.position = ccp(bush.position.x +
+            // move the bush
+            
+            bush.position = ccp(bush.position.x -
                                 
-                                2 * bush.contentSize.width, bush.position.y);
+                                (character.physicsBody.velocity.x * delta), bush.position.y);
+            
+            
+            
+            // if the left corner is one complete width off the screen,
+            
+            // move it to the right
+            
+            if (bush.position.x <= (-1 * bush.contentSize.width)) {
+                
+                bush.position = ccp(bush.position.x +
+                                    
+                                    2 * bush.contentSize.width, bush.position.y);
+                
+            }
+            
+            
             
         }
         
-    
-    
-  }
-    
-    // move and loop the clouds
-    
-    for (CCNode *cloud in _clouds) {
+        // move and loop the clouds
         
-        // move the cloud
-        
-        cloud.position = ccp(cloud.position.x -
-                             
-                             (character.physicsBody.velocity.x * delta), cloud.position.y);
-        
-        // if the left corner is one complete width off the screen, 
-        
-        // move it to the right
-        
-        if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
+        for (CCNode *cloud in _clouds) {
             
-            cloud.position = ccp(cloud.position.x + 
+            // move the cloud
+            
+            cloud.position = ccp(cloud.position.x -
                                  
-                                 2 * cloud.contentSize.width, cloud.position.y);
+                                 (character.physicsBody.velocity.x * delta), cloud.position.y);
+            
+            // if the left corner is one complete width off the screen,
+            
+            // move it to the right
+            
+            if (cloud.position.x <= (-1 * cloud.contentSize.width)) {
+                
+                cloud.position = ccp(cloud.position.x +
+                                     
+                                     2 * cloud.contentSize.width, cloud.position.y);
+                
+            }
             
         }
-        
-    }
+}
+    
     
     NSMutableArray *offScreenObstacles = nil;
     
